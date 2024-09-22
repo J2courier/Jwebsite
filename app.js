4dfe3c4e92d8424b083f19118689a69b524c6723
J2 <149977907+J2courier@users.noreply.github.com>
2024-09-23 07:44:00 +0800

    latest commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const clickProject1 = document.querySelector(".project1");
 const sectionName = document.querySelector(".name");
+const imagePfp = document. querySelector(".container-imgpfp > img");
 const welcome = document.querySelector(".welcome");
 
+imagePfp.style.transition = "opacity 0.3s ease, visibility 0.3s ease";
 sectionName.style.transition = "opacity 0.3s ease, visibility 0.3s ease";
 welcome.style.transition = "opacity 0.3s ease, visibility 0.3s ease";
 
@@ -10,11 +12,15 @@ clickProject1.addEventListener("click", ()=>{
         sectionName.style.visibility = "visible";
         sectionName.style.opacity = "1";
         welcome.style.visibility = "hidden";
+        imagePfp.style.visibility = "hidden";
         welcome.style.opacity = "0";
     } else {
         sectionName.style.visibility = "hidden";
         sectionName.style.opacity = "0";
         welcome.style.visibility = "visible";
+        imagePfp.style.visibility = "visible";
+        imagePfp.style.opacity = "1";
         welcome.style.opacity= "1";
+
     }
-});
+}); 
